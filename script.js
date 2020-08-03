cfaaaaf27b49c640be664c531ee3c9431f7fe4c2
elizabethkerubo02 <68223541+elizabethkerubo02@users.noreply.github.com>
2020-08-03 08:39:06 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+console.log('mnnm');
+let theme =localStorage.getItem('theme');
+
+if (theme == null){
+    setTheme('light')
+}else{
+    setTheme(theme)
+}
+
+
+let themeDots = document.getElementsByClassName("theme-dot");
+
+for(i=0; themeDots.length>i;i++){
+    themeDots[i].addEventListener('click',function(){
+        let mode = this.dataset.mode
+        console.log('Option clicked:', mode);
+        setTheme(mode)
+
+
+    });
+}
+
+function setTheme(mode){
+    if(mode =='light'){
+        document.getElementById('theme-style').href='default.css'
+    }
+    if(mode =='blue'){
+        document.getElementById('theme-style').href= 'blue.css'
+    }
+    if(mode =='mint-green'){
+        document.getElementById('theme-style').href= 'green.css'
+    }
+    if(mode =='pink'){
+        document.getElementById('theme-style').href= 'pink.css'
+    }
+    localStorage.setItem('theme' ,mode)
+}
